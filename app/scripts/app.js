f07f472f522bd3e4c22edcd426fb5f9bbd867f9b
David Sires <d.sires@yahoo.com>
2017-04-19 19:08:00 -0700

    Update-3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,7 @@
         $locationProvider
             .html5Mode({
                 enabled: true,
-                requireBase: false
+                requireBase: false,
             });
 
         $stateProvider
